18a5cf745fd58556f5d16ae6067b7f317f018314
DooMin <tnals2832@naver.com>
2023-09-07 03:18:15 +0900

    second commit

--- src/main/java/com/mycom/word/WordCRUD.java
+++ src/main/java/com/mycom/word/WordCRUD.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class WordCRUD implements ICRUD{
+
     ArrayList<Word> list;
     Scanner s;
     final String fname = "Dictionary.txt";
