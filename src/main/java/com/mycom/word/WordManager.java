d63856cb23f8e1871ae5210577a05bfcf8479901
DooMin <tnals2832@naver.com>
2023-09-08 11:05:39 +0900

    fourth commit

--- src/main/java/com/mycom/word/WordManager.java
+++ src/main/java/com/mycom/word/WordManager.java
@@ -29,7 +29,11 @@ public class WordManager {
         wordCRUD.loadFile();
         while(true){
             int menu = selectMenu();
-            if(menu == 0) break;
+            if(menu == 0){
+                System.out.println("프로그램 종료!! 다음에 만나요~ ");
+                break;
+            }
+
             if(menu == 4){
                 wordCRUD.addItem();
             }
